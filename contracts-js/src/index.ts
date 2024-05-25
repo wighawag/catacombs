364b36d962db181af2ef2e00656847b615e709a4
wighawag <wighawag@gmail.com>
2024-05-25 19:41:06 +0100

    EVM runner + export bytecode

--- contracts-js/src/index.ts
+++ contracts-js/src/index.ts
@@ -1,8 +1,32 @@
 import {EVM} from '@ethereumjs/evm';
-import {bytesToHex, hexToBytes} from '@ethereumjs/util';
+import {Address, bytesToHex, hexToBytes} from '@ethereumjs/util';
 
-export async function createEVMRunner({contracts}: {contracts: string}) {
+export async function createEVMRunner(contracts: {[id: string]: {bytecode: `0x${string}`; argsData?: `0x${string}`}}) {
 	const evm = await EVM.create();
+	const contractNames = Object.keys(contracts);
+	const contractAddresses: {[name: string]: `0x${string}`} = {};
+	for (const contractName of contractNames) {
+		const {bytecode, argsData} = contracts[contractName];
+		const results = await evm.runCall({
+			data: hexToBytes(bytecode + argsData?.slice(2) || ''),
+		});
+		if (!results.createdAddress) {
+			throw new Error(`could not deploy contract ${contractName}`);
+		}
+		const contractAddress = results.createdAddress?.toString() as `0x${string}`;
+		contractAddresses[contractName] = contractAddress;
+	}
+
+	async function runContract(name: string, data: `0x${string}`) {
+		const contractAddress = contractAddresses[name];
+		const results = await evm.runCall({
+			to: Address.fromString(contractAddress),
+			data: hexToBytes(data),
+		});
+
+		return bytesToHex(results.execResult.returnValue);
+	}
+
 	async function runCode(code: `0x${string}`) {
 		return evm
 			.runCode({
@@ -15,6 +39,6 @@ export async function createEVMRunner({contracts}: {contracts: string}) {
 	}
 	return {
 		runCode,
-		// run
+		runContract,
 	};
 }
