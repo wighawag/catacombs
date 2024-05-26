f43bd2ad01fd4e209559924a58bc4c13731935c8
wighawag <wighawag@gmail.com>
2024-05-26 10:14:11 +0100

    local enemy moves

--- contracts/src/game/routes/GameReveal.sol
+++ contracts/src/game/routes/GameReveal.sol
@@ -23,6 +23,12 @@ contract GameReveal is Game {
         uint24 epoch;
     }
 
+    struct Monster {
+        int32 x;
+        int32 y;
+        uint8 life;
+    }
+
     function reveal(uint256 characterID, Game.Action[] calldata actions, bytes32 secret) external {
         Game.Store storage store = getStore();
         Context memory context = _context(store, characterID, actions, secret);
@@ -55,17 +61,54 @@ contract GameReveal is Game {
     function _stateChanges(Context memory context) public pure returns (StateChanges memory stateChanges) {
         uint64 position = context.priorPosition;
         (int32 x, int32 y) = PositionUtils.toXY(position);
+        Monster[5] memory monsters;
+        // TODO randomize
+        monsters[0] = Monster({x: x + 2, y: y + 5, life: 3});
+        monsters[1] = Monster({x: x + 5, y: y + 5, life: 3});
+        monsters[2] = Monster({x: x + 7, y: y + 2, life: 3});
+        monsters[3] = Monster({x: x + 9, y: y + 5, life: 3});
+        monsters[4] = Monster({x: x + 4, y: y + 10, life: 3});
+        // TODO local chest
         Game.Action[] memory actions = context.actions;
         for (uint256 i = 0; i < MAX_PATH_LENGTH; i++) {
             uint64 next = actions[i].position;
             (int32 nextX, int32 nextY) = PositionUtils.toXY(next);
             if (GameUtils.isValidMove(x, y, nextX, nextY)) {
-                // if ()
-                position = next;
+                bool attacked;
+                for (uint256 e = 0; e < 5; e++) {
+                    if (monsters[e].life > 0 && monsters[e].x == nextX && monsters[e].y == nextY) {
+                        attacked = true;
+                        monsters[e].life -= 1;
+                    }
+                }
+                if (!attacked) {
+                    position = next;
+                }
             } else {
                 // TODO should we throw ? or just ignore like now
             }
-            // TODO move enemies
+            (x, y) = PositionUtils.toXY(position);
+            for (uint256 e = 0; e < 5; e++) {
+                Monster memory monster = monsters[e];
+                // TODO prevent monster to share space
+                if (monster.life > 0) {
+                    int32 m_nextX = monster.x;
+                    int32 m_nextY = monster.y;
+                    int32 xDiff = x - monster.x;
+                    int32 yDiff = y - monster.y;
+                    if (xDiff > yDiff) {
+                        m_nextX -= (xDiff / -xDiff);
+                    } else {
+                        m_nextY -= (yDiff / -yDiff);
+                    }
+                    if (m_nextX == x && nextY == y) {
+                        // Player life ---
+                    } else {
+                        monster.x = m_nextX;
+                        monster.y = m_nextY;
+                    }
+                }
+            }
         }
         stateChanges.newPosition = position;
     }
