0e345f7db4b5cc9b17eb2e8597d79e977c6a0ac8
wighawag <wighawag@gmail.com>
2024-06-21 15:53:47 +0100

    canvas fit

--- web/src/lib/ui/screens/game/MainGameScreen.svelte
+++ web/src/lib/ui/screens/game/MainGameScreen.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
 	import WebGlCanvas from '$lib/render/WebGLCanvas.svelte';
 	import {type GameView} from '$lib/state/ViewState';
+	import WelcomeProfile from '../headers/WelcomeProfile.svelte';
 	import Overlay from './Overlay.svelte';
 	export let gameView: GameView;
 </script>
 
-<div class="canvas">
-	<WebGlCanvas state={gameView} />
-</div>
+<header>
+	<WelcomeProfile />
+</header>
+
+<main>
+	<div class="canvas">
+		<WebGlCanvas state={gameView} />
+	</div>
+</main>
+
+<footer>
+	<p>
+		Follow
+		<a href="https://twitter.com/etherplay" target="_blank" rel="noopener noreferrer">@Etherplay</a>
+		for updates
+	</p>
+</footer>
 
 <Overlay {gameView} />
 
 <style>
 	.canvas {
 		pointer-events: none;
-		position: absolute;
-		top: 0;
-		left: 0;
+		/* position: absolute; */
+		/* top: 0;
+		left: 0; */
 		height: 100%;
 		width: 100%;
 	}
