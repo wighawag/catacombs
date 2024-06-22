e0b0cbc52d3219409960321ab9d41a5f18146f92
wighawag <wighawag@gmail.com>
2024-06-22 10:22:39 +0100

    Battle!

--- web/src/lib/ui/screens/game/BattleOverlay.svelte
+++ web/src/lib/ui/screens/game/BattleOverlay.svelte
@@ -8,9 +8,7 @@
 		<img alt="skeleton" src="/images/monsters/skeleton.png" />
 	</div>
 	<div class="actions">
-		<button>Attack</button>
-		<button>Escape</button>
-		<button>Call For Help</button>
+		<button>Battle!</button>
 	</div>
 </div>
 
@@ -33,16 +31,24 @@
 	.monster {
 		display: flex;
 		justify-content: center;
+		/* background-color: red; */
 	}
 
 	.monster img {
 		width: 256px;
 		image-rendering: pixelated;
-		filter: sepia() hue-rotate(115deg) brightness(100%) saturate(200%);
+		filter: sepia() hue-rotate(50deg) brightness(80%);
+		/* opacity: 0.7; */
 	}
 
 	.actions {
 		display: flex;
 		justify-content: center;
+	}
+
+	.actions button {
+		width: 100%;
+		height: 2rem;
+		font-size: 1.2rem;
 	}
 </style>
