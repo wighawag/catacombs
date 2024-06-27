9f8d86be4e7b2d1a85db85225bb5195d3f8923b6
wighawag <wighawag@gmail.com>
2024-06-27 18:28:50 +0100

    remove tombs

--- web/src/lib/render/layers/GroundLayer.ts
+++ web/src/lib/render/layers/GroundLayer.ts
@@ -242,7 +242,7 @@ export class GroundLayer extends Textured2DProgram {
 			// const hy = monster.y;
 
 			if (monster.hp <= 0) {
-				drawTile(this.attributes, hx + 5 / 22, hy + 3 / 22, texPerSprites['tomb.png'], 16 / 28, 16 / 28, 1);
+				// drawTile(this.attributes, hx + 5 / 22, hy + 3 / 22, texPerSprites['tomb.png'], 16 / 28, 16 / 28, 1);
 			} else {
 				drawTile(this.attributes, hx + 5 / 22, hy + 3 / 22, texPerSprites['skeleton_sword.png'], 16 / 28, 16 / 28, 1);
 			}
