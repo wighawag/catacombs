0f6f89bd00e73bf6c20f32435121846428cfac89
wighawag <wighawag@gmail.com>
2024-08-23 13:42:25 +0100

    add contractState to window

--- web/src/lib/state/IndexedState.ts
+++ web/src/lib/state/IndexedState.ts
@@ -2,3 +2,7 @@ import {initContractState} from '$lib/blockchain/contractState';
 import {connection} from './connection';
 
 export const contractState = initContractState(connection);
+
+if (typeof window != 'undefined') {
+	(window as any).contractState = contractState;
+}
